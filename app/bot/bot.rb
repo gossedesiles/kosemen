dab14a5924221e7319afe83fad3c543580904de8
EC2 Default User <ec2-user@ip-172-31-42-0.us-east-2.compute.internal>
2019-01-07 11:08:17 +0000

    test

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,7 +4,7 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(
   access_token: environment["ACCES_TOKEN"],
-  subscribed_fields: %w[histoiredejouir]
+  subscribed_fields: %w[histoire de jouir]
 )
 
 Bot.on :message do |message|
